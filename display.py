095d3faa96a2c09fc2b3d68363aa444f73aa3012
Blayne Wesneski <bwesneskibusiness@gmail.com>
2024-07-29 14:40:42 -0400

    Modified text displaying current time  - Put small outline around the text  - placed drop shadow behind the text  - Changed format to be 12hr instead of 24 hr

--- display.py
+++ display.py
@@ -48,14 +48,23 @@ while running:
     
 
     #Display time
-    now = datetime.now().strftime("%m/%d/%Y %H:%M:%S")
-    font = pygame.font.Font(None, 36)
+    now = datetime.now().strftime("%m/%d/%Y %I:%M %p")
+    font = pygame.font.Font("./ChakraPetch-Bold.ttf", 60)
     time_surface = font.render(now, True, (255, 255, 255))
+    time_shadow = font.render(now, True, (0, 0, 0))
+    time_outline = font.render(now, True, (0, 0, 0))
 
     #Calculate time location
     time_loc = time_surface.get_rect()
     time_loc.topleft = (10, 10)
+    time_shadow_loc = time_shadow.get_rect()
+    time_shadow_loc.topleft = (14, 14)
 
+    outline_loc = [(x,y) for x in (-1,0,1) for y in (-1,0,1) if (x,y) != (0,0)]
+    for offset in outline_loc:
+        screen.blit(time_outline, (time_loc.left + offset[0], time_loc.top + offset[1]))
+
+    screen.blit(time_shadow, time_shadow_loc)
     screen.blit(time_surface, time_loc)
 
     pygame.display.flip()
